925fb1134116ff79d767cbd406b16bdc7c5023a1
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-12-01 11:39:49 +0100

    Q4 & 5

--- cojez_tp7.sql
+++ cojez_tp7.sql
@@ -5,19 +5,44 @@
 SELECT * FROM Appel;
 
 CREATE OR REPLACE FUNCTION AbonnementAdsl() RETURNS VOID AS $$
-	DECLARE
-		appelbad integer;
-	BEGIN
-		FOR appelbad IN SELECT codappel FROM Appel LEFT JOIN Client ON (Appel.idclient = Client.idclient)
-			WHERE cout != 0 AND typecontrat = 'Abonnement Adsl'
-		LOOP
-			UPDATE Appel 
-				SET cout = 0 WHERE codappel = appelbad;
-		END LOOP;
-	END; $$ LANGUAGE PLPGSQL;
+    DECLARE
+        appelbad integer;
+    BEGIN
+        FOR appelbad IN SELECT codappel FROM Appel LEFT JOIN Client ON (Appel.idclient = Client.idclient)
+            WHERE cout != 0 AND typecontrat = 'Abonnement Adsl'
+        LOOP
+            UPDATE Appel 
+                SET cout = 0 WHERE codappel = appelbad;
+        END LOOP;
+    END; $$ LANGUAGE PLPGSQL;
 		
 SELECT * FROM AbonnementAdsl();
 SELECT * FROM Appel;
 
-		
-		--Q2 modifier operations pas problemes
+--E1Q2
+
+--CREATE OR REPLACE FUNCTION RemplirOperations() RETURNS VOID AS $$
+--    DECLARE 
+        
+        --Q2 modifier operations pas problemes
+
+--E1Q3
+
+--E1Q4
+
+CREATE OR REPLACE FUNCTION nbAppels(d date) RETURNS INT AS $$
+    BEGIN
+        RETURN COUNT(codappel) FROM Appel GROUP BY DateOuverture HAVING DateOuverture=d;
+    END; $$ LANGUAGE PLPGSQL;
+
+SELECT * FROM nbAppels('2003-10-10');
+
+--E1Q5
+
+CREATE OR REPLACE FUNCTION coutAppelsOperations(d date) RETURNS INT AS $$
+    BEGIN
+        RETURN SUM(cout) FROM (
+            SELECT cout FROM Appel WHERE DateOuverture=d
+            UNION 
+            SELECT cout FROM Operation WHERE Date=d) AS couts;
+    END; $$ LANGUAGE PLPGSQL;
